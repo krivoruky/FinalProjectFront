93356f09274863dfef654e68c2bc59d9b1dcbfa8
Stas Tikhomirov <73437477+krivoruky@users.noreply.github.com>
2022-06-27 00:11:51 +0300

    Update Article.jsx

--- app/src/components/Articles/Article/Article.jsx
+++ app/src/components/Articles/Article/Article.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import s from './Article.module.sass';
-import img from '../../../media/Man3.png';
+import img from '../../../media/Man2.png';
 import Description from '../../UI/Description';
 
 export default function Article({ name, comment, date }) {
